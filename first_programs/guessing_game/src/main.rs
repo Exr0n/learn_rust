c48d18bf3d91c1199cfe91a0393081f96097b5c7
Exr0n <25516241+Exr0n@users.noreply.github.com>
2020-01-05 09:38:37 -0800

    finish guessing game

--- first_programs/guessing_game/src/main.rs
+++ first_programs/guessing_game/src/main.rs
@@ -6,21 +6,25 @@ fn main() {
     println!("Number Guessing Game");
 
     let secret_number = rand::thread_rng().gen_range(1, 101);
-    println!("The secret number is {}", secret_number);
 
-    println!("Please input your guess:");
-    let mut guess = String::new();
-    io::stdin().read_line(&mut guess) // input
-        .expect("Failed to read line!"); // throws string if enum returned by read_line has type Error
+    loop {
+        println!("Please input your guess:");
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess) // input
+            .expect("Failed to read line!"); // throws string if enum returned by read_line has type Error
 
-    let guess: u32 = guess.trim().parse() // shadowing is legal
-        .expect("Failed to parse number!"); // trim or parse failed
-    
-    println!("You guessed: {}", guess);
+        let guess: u32 = guess.trim().parse() // shadowing is legal
+            .expect("Failed to parse number!"); // trim or parse failed
+        
+        println!("You guessed: {}", guess);
 
-    match guess.cmp(&secret_number) { // switch statement to compare
-        Ordering::Less => println!("That was too small!"),
-        Ordering::Greater => println!("That was too big!"),
-        Ordering::Equal => println!("You win!"),
+        match guess.cmp(&secret_number) { // switch statement to compare
+            Ordering::Less => println!("That was too small!"),
+            Ordering::Greater => println!("That was too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
     }
 }
