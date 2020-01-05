b33689be7e2fd9ab2324dd9690270233cfc47946
Exr0n <25516241+Exr0n@users.noreply.github.com>
2020-01-05 14:44:09 -0800

    Enums

--- first_programs/enums/src/main.rs
+++ first_programs/enums/src/main.rs
@@ -1,12 +1,29 @@
-enum IpAddrKind {
-    V6 {
-        addr: String
-    },
-    V4(u8, u8, u8, u8),
-}
-fn main() {
-    let four = IpAddrKind::V4;
-    let six = IpAddrKind::V6;
 
-    println!("Hello, world!");
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter{ year: u32 },
+}
+impl Coin {
+    fn value(&self) -> u32 {
+        match self {
+            Coin::Penny => 1,
+            Coin::Nickel => 5,
+            Coin::Dime => 10,
+            Coin::Quarter{year} => {
+                println!("Quarter from {}!", year);
+                25
+            }
+        }
+    }
+}
+
+fn main() {
+
+    let my_coin = Coin::Dime;
+    let my_other_coin = Coin::Quarter{year: 2006};
+
+    println!("Value 1: {}", my_coin.value());
+    println!("Value 2: {}", my_other_coin.value());
 }
