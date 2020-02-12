d6f9d734f4d1927e1b18caf31d85eef83d850268
Exr0n <spotyie@gmail.com>
2020-02-11 22:26:15 -0800

    panic

---
+++ first_programs/panic/target/debug/deps/panic-1ca1ee4ebb01fa40.d
@@ -0,0 +1,5 @@
+/Users/lamnguyen/Desktop/projects/learn_rust/first_programs/panic/target/debug/deps/panic-1ca1ee4ebb01fa40: src/main.rs
+
+/Users/lamnguyen/Desktop/projects/learn_rust/first_programs/panic/target/debug/deps/panic-1ca1ee4ebb01fa40.d: src/main.rs
+
+src/main.rs:
---
+++ first_programs/panic/target/debug/panic.d
@@ -0,0 +1,1 @@
+/Users/lamnguyen/Desktop/projects/learn_rust/first_programs/panic/target/debug/panic: /Users/lamnguyen/Desktop/projects/learn_rust/first_programs/panic/src/main.rs
