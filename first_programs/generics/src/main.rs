b55160aa40d816dad0e9a635432f50bed4454a78
Exr0n <spotyie@gmail.com>
2020-02-13 17:27:23 -0800

    generics and traits

--- first_programs/generics/src/main.rs
+++ first_programs/generics/src/main.rs
@@ -1,13 +1,21 @@
+use std::cmp::PartialOrd;
+
 fn main() {
     let number_list = vec![34, 50, 25, 100, 65];
+    let char_list = vec!['y', 'e', 'l', 'l', 'o', 'w'];
 
-    let mut largest = number_list[0];
+    let largest_number = largest(&number_list);
 
-    for number in number_list {
-        if number > largest {
+    println!("The largest number is {}", largest_number);
+    println!("The largest char is {}", largest(&char_list));
+}
+
+fn largest<T: PartialOrd>(list: &[T]) -> &T {
+    let mut largest = &list[0];
+    for number in list {
+        if *number > *largest {
             largest = number;
         }
     }
-
-    println!("The largest number is {}", largest);
+    largest
 }
